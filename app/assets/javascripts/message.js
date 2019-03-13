450a4effdeedc228bcc19c57cac33da5071ebd29
tetsu-26 <heiwa326427@gmial.com>
2019-03-13 20:38:24 +0900

    自動更新

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,6 +1,6 @@
 $(document).on('turbolinks:load', function() {
   $(function(){
-    function buildHTML(message){
+    function buildSendMessageHTML(message){
       var img = message.image ? `<img src=${ message.image } >` : "";
       var html = `<div class="submit" data-id=${ message.id}>
                     <ul class="submit--record">
@@ -31,7 +31,7 @@ $(document).on('turbolinks:load', function() {
         contentType: false
       })
       .done(function(data){
-        var html = buildHTML(data);
+        var html = buildSendMessageHTML(data);
         $('.in-rightcontent').append(html)
         $("html,body").animate({scrollTop: $('.in-rightcontent')[0].scrollHeight}, 'fast');
         $('form')[0].reset();
@@ -41,19 +41,19 @@ $(document).on('turbolinks:load', function() {
       })
     })
 
-    setInterval(reload, 1000*5);
+    var new_message_insert = setInterval(reload, 1000*5);
     function reload(){
-      var message_id = $('.submit:last').data('id');
+      var new_message_id = $('.submit:last').data('id');
       $.ajax({
         type: 'GET',
         url: location.href,
-        data: {id: message_id},
+        data: {id: new_message_id},
         dataType: 'json'
       })
       .done(function(new_messages) {
         if(new_messages.length != 0 ){
           new_messages.forEach(function(new_message) {
-            var AddHTML = buildHTML(new_message);
+            var AddHTML = buildSendMessageHTML(new_message);
             $('.in-rightcontent').append(AddHTML);
           })
           $("html,body").animate({scrollTop: $('.in-rightcontent')[0].scrollHeight}, 'fast');
