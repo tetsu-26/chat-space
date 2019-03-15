b562e23f36d2aaaf1b1a3f4f2ca45330106f8886
tetsu-26 <heiwa326427@gmial.com>
2019-03-15 16:19:51 +0900

    デプロイ

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -59,4 +59,4 @@
 #     auth_methods: %w(publickey password)
 #     # password: "please use keys"
 #   }
-server 'http://13.115.168.44', user: 'ec2-user', roles: %w{app db web}
+server '13.115.168.44', user: 'ec2-user', roles: %w{app db web}
